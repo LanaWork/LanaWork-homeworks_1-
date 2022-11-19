193f5212850f8bee6ebb211dd14dca43a8b3c4d0
LanaWork <shilova.s@mail.ru>
2022-11-19 22:00:28 +0300

    new

--- homeworks_1/Program.cs
+++ homeworks_1/Program.cs
@@ -34,7 +34,7 @@ Console.WriteLine(max);
 */
 /*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 */
-
+/*
 Console.WriteLine("Введи число");
 int num;
 num = Convert.ToInt32(Console.ReadLine());
@@ -46,19 +46,16 @@ else
 {
    Console.WriteLine("не четное число"); 
 }
-
+*/
 /*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
 
-/*
-Console.Write("Write number 1: ");
-int n = int.Parse(Console.ReadLine());
-Console.Write("Write number 2: ");
-int m = int.Parse(Console.ReadLine());
-for (int i = n; i <= m; i++)
-
+Console.Write("Введи число N ");
+int m = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= m; i++)
+{
 if (i % 2 == 0) 
 {
 Console.Write(i + " ");
 }
-
-Console.ReadKey();*/
+}
+Console.ReadKey();
--- homeworks_1/obj/Debug/net7.0/homeworks_1.AssemblyInfo.cs
+++ homeworks_1/obj/Debug/net7.0/homeworks_1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("homeworks1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("homeworks_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("homeworks1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("homeworks1")]
+[assembly: System.Reflection.AssemblyProductAttribute("homeworks_1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("homeworks_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
